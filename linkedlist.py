d76e63f2cacbb413040a8f779494796c44ace4c7
Soundaryagomathy <soundaryagomathyp@gmail.com>
2026-09-12 22:47:10 +0530

    added

--- linkedlist.py
+++ linkedlist.py
@@ -1,1 +1,12 @@
 #linked list
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+n1=Node(10)
+n2=Node(20)
+n1.next=n2
+current=n1
+while current is not None:
+    print (current.data)
+    current=current.next
